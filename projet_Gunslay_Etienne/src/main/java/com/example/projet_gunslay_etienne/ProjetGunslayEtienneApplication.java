ab3d644d10861aa3dd96a8ae2deaa37323f510cc
Etienne <etienne.gunslay@gmail.com>
2025-11-28 12:29:50 +0100

    build: Architecture de l'app

---
+++ projet_Gunslay_Etienne/src/main/java/com/example/projet_gunslay_etienne/ProjetGunslayEtienneApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_gunslay_etienne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGunslayEtienneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetGunslayEtienneApplication.class, args);
+    }
+
+}
---
+++ projet_Gunslay_Etienne/src/test/java/com/example/projet_gunslay_etienne/ProjetGunslayEtienneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_gunslay_etienne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGunslayEtienneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
